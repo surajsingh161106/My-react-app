e0bf3b100c768b958a86e5d84a63528e8dde1ef4
surajsingh161106 <suraj.sin2006@gmail.com>
2024-11-06 17:08:29 +0530

    Add Lowercase Button

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,6 +7,13 @@ export default function Textform(props) {
     let newText = text.toUpperCase(); 
     setText(newText)
   }
+
+  const handleLoClick = ()=>{
+    // console.log("Lowercase was Clicked: " + text); // Only for Check Bugs nothing Alse
+    let newText = text.toLowerCase(); 
+    setText(newText)
+  }
+
   const handleOnChange = (event)=>{
     // console.log("On Change"); // Only for Check Bugs nothing Alse
     setText(event.target.value);  
@@ -17,12 +24,22 @@ export default function Textform(props) {
   // setText("new Txt"); // Correct way to change the state
 
   return (
-    <div>
+    <>
+    <div className="container">
       <h1>{props.heading}</h1>
         <div className="mb-3">
         <textarea className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
     </div>
+    <div className="container my-3">
+      <h1>Your Text Summary</h1>
+      <p>{text.split(" ").length} word and {text.length} character</p>
+      <p>{0.008 * text.split(" ").length} Minutes Read</p>
+      <h2>Preview</h2>
+      <p>{text}</p>
+    </div>
+    </>
   )
 }
